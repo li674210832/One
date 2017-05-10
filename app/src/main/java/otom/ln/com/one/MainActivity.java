4cdf293a19c9e612d3bfde85d75a69601d869bc0
LiNing <674210832@qq.com>
2017-05-10 21:16:05 +0800

    meizhi

--- app/src/main/java/otom/ln/com/one/MainActivity.java
+++ app/src/main/java/otom/ln/com/one/MainActivity.java
@@ -2,6 +2,7 @@ package otom.ln.com.one;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
-
     }
+    
 }
